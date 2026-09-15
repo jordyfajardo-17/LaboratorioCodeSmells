4575a3ea877900bf287ee09eea96981c7c0d3a2f
Jordy Fajardo <jordy.fajardo@uees.edu.ec>
2026-09-14 23:03:40 -0500

    chore: registrar codigo inicial y linea base

---
+++ src/main/java/edu/uees/laboratorio/Estudiante.java
@@ -0,0 +1,13 @@
+package edu.uees.laboratorio;
+
+public class Estudiante {
+    private final String nombre;
+
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+}
