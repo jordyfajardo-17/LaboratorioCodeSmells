812a9260067eb721acf22c32f743a48b6d0c7ff3
Jordy Fajardo <jordy.fajardo@uees.edu.ec>
2026-09-15 21:53:28 -0500

    refactor: nombrar regla de anticipacion

--- src/main/java/edu/uees/laboratorio/ServicioTutorias.java
+++ src/main/java/edu/uees/laboratorio/ServicioTutorias.java
@@ -1,6 +1,7 @@
 package edu.uees.laboratorio;
 
 public class ServicioTutorias {
+    private static final int HORAS_MINIMAS_CONFIRMACION = 2;
 
     public void confirmarReserva(Reserva reserva, int horasAnticipacion) {
 
@@ -10,7 +11,7 @@ public class ServicioTutorias {
 
                 if (!reserva.isCancelada()) {
 
-                    if (horasAnticipacion >= 2) {
+                   if (horasAnticipacion >= HORAS_MINIMAS_CONFIRMACION) {
 
                         System.out.println("Procesando " + reserva.getId());
 
